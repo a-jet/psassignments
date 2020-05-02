776af76dc5f558b0219c9021d2ef21603261d27d
Teja <tejamarisetti365@gmail.com>
2020-05-02 10:40:38 +0530

    WeekFourCommit

--- com/sapient/solution3/Calculate.java
+++ com/sapient/solution3/Calculate.java
@@ -1,24 +1,23 @@
 package com.sapient.solution3;
 
 enum Calculate implements NumProcess{
-	A{
-		public double cal(double a,double b) {
-			return a+b;
-		}
-	},
-	S{
-		public double cal(double a,double b) {
-			return a-b;
-		}
-	},
-	M{
-		public double cal(double a,double b) {
-			return a*b;
-		}
-	},
-	D{
-		public double cal(double a,double b) {
-			return a/b;
-		}
+	A((a,b)->a+b),
+	S((a,b)->a-b),
+	M((a,b)->a*b),
+	D((a,b)->a/b);
+	NumProcess n;
+	Calculate(final NumProcess n) {
+		this.n=n;
 	}
+	public double cal(double a,double b) {
+		return n.cal(a, b);
+	}
+	/*Internally, code execution is done like this
+	enum Object is declared in NumProcessRunner.java,
+	Calculate c=Calculate.A;
+	Constructor is called, n is assigned and cal method is defined by lambda expression,
+	NumProcess n=((x,y)->x+y);
+	Output is displayed on console,
+	System.out.println(c.cal(a, b));
+	*/
 }
